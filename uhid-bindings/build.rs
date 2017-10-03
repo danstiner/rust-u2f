df8d7c37b1641d8c40b7cb8b6369110f8f93627a
Daniel Stiner <danstiner@gmail.com>
2017-10-02 18:32:56 -0700

    Remove uneccessary link against bz2

--- uhid-bindings/build.rs
+++ uhid-bindings/build.rs
@@ -4,10 +4,6 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    // Tell cargo to tell rustc to link the system bzip2
-    // shared library.
-    println!("cargo:rustc-link-lib=bz2");
-
     // The bindgen::Builder is the main entry point
     // to bindgen, and lets you build up options for
     // the resulting bindings.
