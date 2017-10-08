00b6bc3d6e3b6767c43eca7aef3ff4c66420aa6e
Daniel Stiner <danstiner@gmail.com>
2017-10-08 00:20:42 -0700

    Add basic definitions for u2f messages

--- u2fhid-transport/src/lib.rs
+++ u2fhid-transport/src/lib.rs
@@ -1,6 +1,45 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
+enum StatusCode {
+    NoError,
+    TestOfUserPresenceNotSatisfied,
+    InvalidKeyHandle,
+    RequestLengthInvalid,
+    RequestClassNotSupported,
+    RequestInstructionNotSuppored,
+}
+
+enum AuthenticateControlCode {
+    CheckOnly,
+    EnforceUserPresenceAndSign,
+    DontEnforceUserPresenceAndSign,
+}
+
+enum RequestMessage {
+    Register {
+        challenge_parameter: [u8; 32],
+        application_parameter: [u8; 32],
+    },
+    Authenticate {
+        control_code: AuthenticateControlCode,
+        challenge_parameter: [u8; 32],
+        application_parameter: [u8; 32],
+        key_handle: Vec<u8>,
+    },
+    GetVersion,
+}
+
+enum ResponseMessage {
+    RegisterSuccess {
+        user_public_key: [u8; 65],
+        key_handle: Vec<u8>,
+        attestation_certificate: Vec<u8>,
+        signature: Vec<u8>
+    },
+    AuthenticateSuccess {
+        user_present: bool,
+        counter: u32,
+        signature: Vec<u8>,
+    },
+    Version {
+        version_string: String,
     }
 }
