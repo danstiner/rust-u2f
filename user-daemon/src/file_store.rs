6a5fb96db1e667a6d52e7cfd06c581797992d41d
Daniel Stiner <danstiner@gmail.com>
2018-01-22 21:58:29 -0800

    Load user data upon each use
    Instead of once on program launch
    
    Fixes #14

--- user-daemon/src/file_store.rs
+++ user-daemon/src/file_store.rs
@@ -1,7 +1,6 @@
 use futures::{Future, IntoFuture};
 use futures::future;
 use serde_json;
-use std::cell::RefCell;
 use std::collections::HashMap;
 use std::fs;
 use std::fs::{File, OpenOptions};
@@ -12,6 +11,13 @@ use std::path::PathBuf;
 
 use u2f_core::{AppId, ApplicationKey, Counter, KeyHandle, SecretStore};
 
+macro_rules! tryf {
+    ($e:expr) => (match $e {
+        Ok(t) => t,
+        Err(e) => return Box::new(future::err(From::from(e))),
+    })
+}
+
 #[derive(Serialize, Deserialize)]
 struct Data {
     application_keys: HashMap<AppId, ApplicationKey>,
@@ -20,28 +26,24 @@ struct Data {
 
 pub struct FileStore {
     path: PathBuf,
-    data: RefCell<Data>,
 }
 
 impl FileStore {
     pub fn new(path: PathBuf) -> io::Result<FileStore> {
-        let data = Self::load(&path)?;
         Ok(FileStore {
             path: path,
-            data: RefCell::new(data),
         })
     }
 
-    fn save(&self) -> io::Result<()> {
-        overwrite_file_atomic(&self.path, |writer| {
-            serde_json::to_writer_pretty(writer, &*self.data.borrow()).unwrap();
-            Ok(())
+    fn save(&self, data: &Data) -> io::Result<()> {
+        overwrite_file_atomic(&self.path, move |writer| {
+            serde_json::to_writer_pretty(writer, &data).map_err(|e| e.into())
         })
     }
 
-    fn load(path: &Path) -> io::Result<Data> {
-        match File::open(path) {
-            Ok(file) => Ok(serde_json::from_reader(file).unwrap()),
+    fn load(&self) -> io::Result<Data> {
+        match File::open(&self.path) {
+            Ok(file) => serde_json::from_reader(file).map_err(|e| e.into()),
             Err(ref err) if err.kind() == io::ErrorKind::NotFound => Ok(Data {
                 application_keys: HashMap::new(),
                 counters: HashMap::new(),
@@ -56,36 +58,31 @@ impl SecretStore for FileStore {
         &self,
         key: &ApplicationKey,
     ) -> Box<Future<Item = (), Error = io::Error>> {
-        self.data
-            .borrow_mut()
-            .application_keys
-            .insert(key.application, key.clone());
-
-        Box::new(self.save().into_future())
+        let mut data = tryf!(self.load());
+        data.application_keys.insert(key.application, key.clone());
+        Box::new(self.save(&data).into_future())
     }
 
     fn get_and_increment_counter(
         &self,
         application: &AppId,
     ) -> Box<Future<Item = Counter, Error = io::Error>> {
-        let value = {
-            let mut data = self.data.borrow_mut();
+        let mut data = tryf!(self.load());
 
-            if !data.counters.contains_key(application) {
-                data.counters.insert(*application, 0);
-            }
+        if !data.counters.contains_key(application) {
+            data.counters.insert(*application, 0);
+        }
 
-            match data.counters.get_mut(application) {
-                Some(counter) => {
-                    let value = *counter;
-                    *counter = value + 1;
-                    value
-                }
-                None => unreachable!(),
+        let value = match data.counters.get_mut(application) {
+            Some(counter) => {
+                let value = *counter;
+                *counter = value + 1;
+                value
             }
+            None => unreachable!(),
         };
 
-        Box::new(self.save().into_future().map(move |_| value))
+        Box::new(self.save(&data).into_future().map(move |_| value))
     }
 
     fn retrieve_application_key(
@@ -93,17 +90,15 @@ impl SecretStore for FileStore {
         application: &AppId,
         handle: &KeyHandle,
     ) -> Box<Future<Item = Option<ApplicationKey>, Error = io::Error>> {
-        let res = match self.data.borrow().application_keys.get(application) {
-            Some(key) => {
-                if key.handle.eq_consttime(handle) {
-                    Some(key.clone())
-                } else {
-                    None
-                }
+        let data = tryf!(self.load());
+        let opt_key = data.application_keys.get(application).and_then(|key| {
+            if key.handle.eq_consttime(handle) {
+                Some(key.clone())
+            } else {
+                None
             }
-            None => None,
-        };
-        Box::new(future::ok(res))
+        });
+        Box::new(future::ok(opt_key))
     }
 }
 
