62f5607c857e379f997a52bc30dfec0eae294371
Daniel Stiner <daniel.stiner@gmail.com>
2019-02-24 22:07:15 -0800

    Update known app IDs

--- u2f-core/src/known_app_ids.rs
+++ u2f-core/src/known_app_ids.rs
@@ -7,21 +7,27 @@ pub fn try_reverse_app_id(app_id: &AppId) -> Option<String> {
     KNOWN_APP_IDS.get(app_id).map(|s| String::from(*s))
 }
 
-/// Should be kept in sync with https://github.com/github/SoftU2F/blob/master/SoftU2FTool/KnownFacets.swift
 lazy_static! {
     static ref KNOWN_APP_IDS: HashMap<AppId, &'static str> = {
         let mut map = HashMap::new();
-        map.insert(from_url("https://github.com/u2f/trusted_facets"), "https://github.com");
-        map.insert(from_url("https://demo.yubico.com"), "https://demo.yubico.com");
-        map.insert(from_url("https://www.dropbox.com/u2f-app-id.json"), "https://dropbox.com");
-        map.insert(from_url("https://www.gstatic.com/securitykey/origins.json"), "https://google.com");
-        map.insert(from_url("https://vault.bitwarden.com/app-id.json"), "https://vault.bitwarden.com");
-        map.insert(from_url("https://keepersecurity.com"), "https://keepersecurity.com");
-        map.insert(from_url("https://api-9dcf9b83.duosecurity.com"), "https://api-9dcf9b83.duosecurity.com");
-        map.insert(from_url("https://dashboard.stripe.com"), "https://dashboard.stripe.com");
-        map.insert(from_url("https://id.fedoraproject.org/u2f-origins.json"), "https://id.fedoraproject.org");
-        map.insert(from_url("https://gitlab.com"), "https://gitlab.com");
-        map.insert(from_url("https://www.fastmail.com"), "https://www.fastmail.com");
+
+        // Should be kept in sync with https://github.com/github/SoftU2F/blob/master/SoftU2FTool/KnownFacets.swift
+        map.insert(from_url("https://github.com/u2f/trusted_facets"), "github.com");
+        map.insert(from_url("https://demo.yubico.com"), "demo.yubico.com");
+        map.insert(from_url("https://www.dropbox.com/u2f-app-id.json"), "dropbox.com");
+        map.insert(from_url("https://www.gstatic.com/securitykey/origins.json"), "google.com");
+        map.insert(from_url("https://vault.bitwarden.com/app-id.json"), "vault.bitwarden.com");
+        map.insert(from_url("https://keepersecurity.com"), "keepersecurity.com");
+        map.insert(from_url("https://api-9dcf9b83.duosecurity.com"), "duosecurity.com");
+        map.insert(from_url("https://dashboard.stripe.com"), "dashboard.stripe.com");
+        map.insert(from_url("https://id.fedoraproject.org/u2f-origins.json"), "id.fedoraproject.org");
+        map.insert(from_url("https://lastpass.com"), "lastpass.com");
+
+        // Additional known app IDs not yet in KnownFacets.swift
+        map.insert(from_url("demo.yubico.com"), "demo.yubico.com");
+        map.insert(from_url("https://gitlab.com"), "gitlab.com");
+        map.insert(from_url("https://www.fastmail.com"), "www.fastmail.com");
+
         map
     };
 }
