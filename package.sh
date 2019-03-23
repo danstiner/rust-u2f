c0dfdc4db2ef220cfd2ac236785fc189ec353312
Daniel Stiner <danstiner@gmail.com>
2019-03-23 14:07:50 -0700

    Support for podman

--- package.sh
+++ package.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 set -euxo pipefail
 
-package_with_docker() {
+if hash podman 2>/dev/null; then
+    docker=podman
+else
+    docker=docker
+fi
+
+package() {
     baseimage=$1
     image=$2
     tag=$3
@@ -9,25 +15,25 @@ package_with_docker() {
     base_dockerfile="Dockerfile.$baseimage"
     build_target="rust-u2f-build-$image:$tag"
     dist_path="dist/$image/$tag/"
-    dist_dockerfile="$dist_path/Dockerfile"
+    dist_dockerfile="${dist_path}Dockerfile"
 
     mkdir -p "$dist_path"
     cat "$base_dockerfile" | sed -e "s/$baseimage:latest/$image:$tag/" > "$dist_dockerfile"
-    docker build . -f "$dist_dockerfile" -t "$build_target"
+    $docker build -f "$dist_dockerfile" -t "$build_target" .
 
-    id=$(docker create "$build_target")
-    docker cp "$id":/app/linux/dist/. "$dist_path"
-    docker rm -v $id
+    id=$($docker create "$build_target")
+    $docker cp "$id":/app/linux/dist/. "$dist_path"
+    $docker rm -v $id
 }
 
 [[ -d dist/ ]] && rm -r dist/
 
-package_with_docker debian debian stretch
+package debian debian stretch
 
-package_with_docker debian ubuntu bionic
-package_with_docker debian ubuntu cosmic
-package_with_docker debian ubuntu disco
-package_with_docker debian ubuntu xenial
+package debian ubuntu bionic
+package debian ubuntu cosmic
+package debian ubuntu disco
+package debian ubuntu xenial
 
-package_with_docker fedora fedora 29
-package_with_docker fedora fedora 28
+package fedora fedora 29
+package fedora fedora 28
