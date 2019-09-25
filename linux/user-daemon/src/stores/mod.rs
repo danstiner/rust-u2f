27914155f7771b95fe0551f1f7067ac140857f59
Daniel Stiner <danstiner@gmail.com>
2019-09-24 23:25:05 -0700

    File store V2

--- linux/user-daemon/src/stores/mod.rs
+++ linux/user-daemon/src/stores/mod.rs
@@ -1,8 +1,8 @@
 use u2f_core::{ApplicationKey, Counter};
 
 pub(crate) mod file;
+pub(crate) mod file_store_v2;
 pub(crate) mod secret_service;
-
 
 #[derive(Clone, Serialize, Deserialize, Debug)]
 pub struct Secret {
