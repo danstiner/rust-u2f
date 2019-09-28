d260a912693d029fa235f655241c56f2e62138e2
Daniel Stiner <danstiner@gmail.com>
2019-09-28 15:09:16 -0700

    Delete unused file store code

--- linux/user-daemon/src/stores/file_store.rs
+++ linux/user-daemon/src/stores/file_store.rs
@@ -38,20 +38,21 @@ impl FileStore {
 
     pub fn iter(&self) -> io::Result<IntoIter<Secret>> {
         let data = self.load()?;
-        let secrets: Vec<Secret> = data.application_keys.values().map(|application_key| {
-            let counter = data.counters.get(&application_key.application).unwrap_or(&0);
-            Secret {
-                application_key: application_key.clone(),
-                counter: *counter,
-            }
-        }).collect();
+        let secrets: Vec<Secret> = data
+            .application_keys
+            .values()
+            .map(|application_key| {
+                let counter = data
+                    .counters
+                    .get(&application_key.application)
+                    .unwrap_or(&0);
+                Secret {
+                    application_key: application_key.clone(),
+                    counter: *counter,
+                }
+            })
+            .collect();
         Ok(secrets.into_iter())
-    }
-
-    fn save(&self, data: &Data) -> io::Result<()> {
-        overwrite_file_atomic(&self.path, move |writer| {
-            serde_json::to_writer_pretty(writer, &data).map_err(|e| e.into())
-        })
     }
 
     fn load(&self) -> io::Result<Data> {
@@ -63,181 +64,5 @@ impl FileStore {
             }),
             Err(err) => Err(err),
         }
-    }
-}
-
-impl SecretStore for FileStore {
-    fn add_application_key(
-        &self,
-        key: &ApplicationKey,
-    ) -> io::Result<()> {
-        let mut data = self.load()?;
-        data.application_keys.insert(key.application, key.clone());
-        self.save(&data)
-    }
-
-    fn get_and_increment_counter(
-        &self,
-        application: &AppId,
-        _handle: &KeyHandle,
-    ) -> io::Result<Counter> {
-        let mut data = self.load()?;
-
-        if !data.counters.contains_key(application) {
-            data.counters.insert(*application, 0);
-        }
-
-        let value = match data.counters.get_mut(application) {
-            Some(counter) => {
-                let value = *counter;
-                *counter = value + 1;
-                value
-            }
-            None => unreachable!(),
-        };
-
-        self.save(&data)?;
-        Ok(value)
-    }
-
-    fn retrieve_application_key(
-        &self,
-        application: &AppId,
-        handle: &KeyHandle,
-    ) -> io::Result<Option<ApplicationKey>> {
-        let data = self.load()?;
-        let opt_key = data.application_keys.get(application).and_then(|key| {
-            if key.handle.eq_consttime(handle) {
-                Some(key.clone())
-            } else {
-                None
-            }
-        });
-        Ok(opt_key)
-    }
-}
-
-fn overwrite_file_atomic<W>(path: &Path, writer_fn: W) -> io::Result<()>
-where
-    W: FnOnce(Box<&mut dyn Write>) -> io::Result<()>,
-{
-    let directory = path.parent().ok_or(io::Error::new(
-        io::ErrorKind::InvalidInput,
-        "Invalid file path, does not have a parent directory",
-    ))?;
-    let tmp_path = make_tmp_path(path)?;
-
-    {
-        let mut tmp_file = OpenOptions::new()
-            .write(true)
-            .create_new(true)
-            .mode(0o600)
-            .open(&tmp_path)?;
-        writer_fn(Box::new(&mut tmp_file))?;
-        tmp_file.flush()?;
-        tmp_file.sync_all()?;
-    }
-
-    fs::rename(&tmp_path, path)?;
-    fsync_dir(directory)?;
-    Ok(())
-}
-
-fn fsync_dir(dir: &Path) -> io::Result<()> {
-    let f = File::open(dir)?;
-    f.sync_all()
-}
-
-fn make_tmp_path(path: &Path) -> io::Result<PathBuf> {
-    let mut tmp_path = PathBuf::from(path);
-    let mut file_name = tmp_path
-        .file_name()
-        .ok_or(io::Error::new(
-            io::ErrorKind::InvalidInput,
-            "Invalid file path, does not end in a file name",
-        ))?
-        .to_owned();
-    file_name.push(".tmp");
-    tmp_path.set_file_name(file_name);
-    Ok(tmp_path)
-}
-
-#[cfg(test)]
-mod tests {
-    extern crate tempdir;
-
-    use u2f_core::PrivateKey;
-
-    use super::*;
-
-    use self::tempdir::TempDir;
-
-    fn fake_app_id() -> AppId {
-        AppId::from_bytes(&vec![0u8; 32])
-    }
-
-    fn fake_key() -> PrivateKey {
-        PrivateKey::from_pem(
-            "-----BEGIN EC PRIVATE KEY-----
-MHcCAQEEICm1nBaPoI3Q3+RJ143W8eCBAdkxrq5YUoNQ9joO0CdroAoGCCqGSM49
-AwEHoUQDQgAE4CiwgIh5tZgW85DKWRajIeTv7Z11C0nmida+m53yVySriU2YK/8O
-i2L2wGDHkWWIJJSthmgwkZovXHyMXMpDhw==
------END EC PRIVATE KEY-----",
-        )
-    }
-
-    fn fake_key_handle() -> KeyHandle {
-        KeyHandle::from(&Vec::new())
-    }
-
-    #[test]
-    fn get_and_increment_counter() {
-        let dir = TempDir::new("file_store_tests").unwrap();
-        let path = dir.path().join("store");
-        let store = FileStore::new(path).unwrap();
-        let app_id = fake_app_id();
-        let handle = fake_key_handle();
-        let key = fake_key();
-        let app_key = ApplicationKey::new(app_id, handle, key);
-        store.add_application_key(&app_key).unwrap();
-
-        let counter0 = store.get_and_increment_counter(&app_id, &app_key.handle).unwrap();
-        let counter1 = store.get_and_increment_counter(&app_id, &app_key.handle).unwrap();
-
-        assert_eq!(counter0 + 1, counter1);
-    }
-
-    #[test]
-    fn retrieve_application_key() {
-        let dir = TempDir::new("file_store_tests").unwrap();
-        let path = dir.path().join("store");
-        let store = FileStore::new(path).unwrap();
-        let app_id = fake_app_id();
-        let handle = fake_key_handle();
-        let key = fake_key();
-        let app_key = ApplicationKey::new(app_id, handle, key);
-        store.add_application_key(&app_key).unwrap();
-
-        let retrieved_app_key = store
-            .retrieve_application_key(&app_key.application, &app_key.handle)
-            .unwrap()
-            .unwrap();
-
-        assert_eq!(retrieved_app_key.application, app_key.application);
-        assert_eq!(retrieved_app_key.handle, app_key.handle);
-        // Skip key field, it is not easily comparable
-    }
-
-    #[test]
-    fn retrieve_nonexistent_key_is_none() {
-        let dir = TempDir::new("file_store_tests").unwrap();
-        let path = dir.path().join("store");
-        let store = FileStore::new(path).unwrap();
-
-        let key = store
-            .retrieve_application_key(&fake_app_id(), &fake_key_handle())
-            .unwrap();
-
-        assert!(key.is_none());
     }
 }
