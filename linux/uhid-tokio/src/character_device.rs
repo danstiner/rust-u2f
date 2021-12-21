b9a2418dda43fcfeccee6a37233d51ccd7c99d60
Daniel Stiner <danstiner@gmail.com>
2021-12-20 23:44:33 -0800

    Fix character device

--- linux/uhid-tokio/src/character_device.rs
+++ linux/uhid-tokio/src/character_device.rs
@@ -1,26 +1,53 @@
 use std::{
-    fmt, io,
+    cmp, fmt,
+    fs::OpenOptions,
+    io,
+    os::unix::prelude::FileTypeExt,
     path::Path,
     pin::Pin,
+    sync::Arc,
     task::{Context, Poll},
 };
 
-use futures::ready;
+use futures::{ready, FutureExt};
 use pin_project::pin_project;
-use tokio::io::{AsyncRead, AsyncWrite, ReadBuf};
+use std::io::{Read, Write};
+use tokio::task::{self, JoinHandle};
+use tracing::trace;
 
-// Character devices do not support seeking
+enum State {
+    Ready(Vec<u8>),
+    Reading(JoinHandle<io::Result<Vec<u8>>>),
+}
+
+/// A reference to a device driver on the system.
+///
+/// Character devices (also known as raw devices)
 #[pin_project]
-pub struct CharacterDevice(#[pin] tokio::fs::File);
+pub struct CharacterDevice {
+    file: Arc<std::fs::File>,
+    state: State,
+}
 
 impl CharacterDevice {
-    pub async fn open(path: &Path) -> Result<Self, io::Error> {
-        let file = tokio::fs::OpenOptions::new()
-            .read(true)
-            .write(true)
-            .open(path)
-            .await?;
-        Ok(Self(file))
+    pub async fn open(path: &Path) -> io::Result<Self> {
+        let path = path.to_owned();
+        let file = task::spawn_blocking(move || {
+            let file = OpenOptions::new().read(true).write(true).open(path)?;
+            if !file.metadata()?.file_type().is_char_device() {
+                return Err(io::Error::new(
+                    io::ErrorKind::Other,
+                    "Not a character device",
+                ));
+            }
+            Ok(file)
+        })
+        .await??;
+
+        Ok(Self {
+            file: Arc::new(file),
+            state: State::Ready(vec![]),
+        })
     }
 }
 
@@ -29,35 +56,91 @@ impl futures::AsyncRead for CharacterDevice {
         self: Pin<&mut Self>,
         cx: &mut Context<'_>,
         buf: &mut [u8],
-    ) -> Poll<Result<usize, io::Error>> {
-        let mut buf = ReadBuf::new(buf);
-        ready!(self.project().0.poll_read(cx, &mut buf))?;
-        Poll::Ready(Ok(buf.filled().len()))
+    ) -> Poll<io::Result<usize>> {
+        trace!("CharacterDevice::read; buf.len:{}", buf.len());
+        let this = self.project();
+        loop {
+            match this.state {
+                State::Ready(ref mut prev_read) => {
+                    if prev_read.len() > 0 {
+                        trace!("CharacterDevice::read Leftover Read");
+                        // Return leftover bytes from a previous read
+                        let n = cmp::min(prev_read.len(), buf.len());
+                        let mut remaining = prev_read.split_off(n);
+                        std::mem::swap(prev_read, &mut remaining);
+                        buf[..n].copy_from_slice(&remaining);
+                        return Poll::Ready(Ok(n));
+                    } else {
+                        trace!("CharacterDevice::read Spawn Read");
+                        // Spawn a new read on a worker thread
+                        // TODO: Consider opening device in async mode instead
+                        // TODO: Re-use read buffer instead of allocating every time
+                        let file = Arc::clone(this.file);
+                        let mut read = vec![0u8; buf.len()];
+                        *this.state = State::Reading(task::spawn_blocking(move || {
+                            (&*file).read(&mut read)?;
+                            Ok(read)
+                        }));
+                    }
+                }
+                State::Reading(ref mut handle) => {
+                    trace!("CharacterDevice::read Reading");
+                    // Check if read is complete
+                    let mut read = ready!(handle.poll_unpin(cx))??;
+
+                    // If it is, copy as many bytes as fit into buf and split off the rest
+                    let n = cmp::min(read.len(), buf.len());
+                    let remaining = read.split_off(n);
+                    buf[..n].copy_from_slice(&read);
+
+                    // Store any remaining bytes that were split off
+                    *this.state = State::Ready(remaining);
+
+                    // Return how many bytes were copied into buf
+                    return Poll::Ready(Ok(n));
+                }
+            }
+        }
     }
 }
 
 impl futures::AsyncWrite for CharacterDevice {
     fn poll_write(
         self: Pin<&mut Self>,
-        cx: &mut Context<'_>,
+        _cx: &mut Context<'_>,
         buf: &[u8],
-    ) -> Poll<Result<usize, io::Error>> {
-        self.project().0.poll_write(cx, buf)
+    ) -> Poll<io::Result<usize>> {
+        trace!("CharacterDevice::write; buf.len:{}", buf.len());
+        // TODO: Async writes
+        let file = Arc::clone(&mut self.project().file);
+        Poll::Ready((&*file).write(buf))
     }
 
-    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), io::Error>> {
-        self.project().0.poll_flush(cx)
+    fn poll_write_vectored(
+        self: Pin<&mut Self>,
+        _cx: &mut Context<'_>,
+        bufs: &[io::IoSlice<'_>],
+    ) -> Poll<io::Result<usize>> {
+        // TODO: Async writes
+        let file = Arc::clone(self.project().file);
+        Poll::Ready((&*file).write_vectored(bufs))
     }
 
-    fn poll_close(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), io::Error>> {
-        self.project().0.poll_shutdown(cx)
+    fn poll_flush(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<io::Result<()>> {
+        let file = Arc::clone(self.project().file);
+        // TODO: Async flush
+        Poll::Ready((&*file).flush())
+    }
+
+    fn poll_close(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<io::Result<()>> {
+        Poll::Ready(Ok(()))
     }
 }
 
 impl fmt::Debug for CharacterDevice {
     fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
         fmt.debug_struct("CharacterDevice")
-            .field("file", &self.0)
+            .field("file", &self.file)
             .finish()
     }
 }
