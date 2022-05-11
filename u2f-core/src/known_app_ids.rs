933b314bf0d438859629cdcc79e8e74589085869
Daniel Stiner <danstiner@gmail.com>
2022-05-10 23:42:56 -0700

    Add www.token2.com to known app ids

--- u2f-core/src/known_app_ids.rs
+++ u2f-core/src/known_app_ids.rs
@@ -29,32 +29,52 @@ pub fn try_reverse_app_id(app_id: &AppId) -> Option<String> {
 lazy_static! {
     static ref KNOWN_APP_IDS: HashMap<AppId, &'static str> = {
         let mut map = HashMap::new();
-
-        // Sourced from https://github.com/github/SoftU2F/blob/master/SoftU2FTool/KnownFacets.swift
-        map.insert(from_url("https://github.com/u2f/trusted_facets"), "github.com");
-        map.insert(from_url("https://demo.yubico.com"), "demo.yubico.com");
-        map.insert(from_url("https://www.dropbox.com/u2f-app-id.json"), "dropbox.com");
-        map.insert(from_url("https://www.gstatic.com/securitykey/origins.json"), "google.com");
-        map.insert(from_url("https://vault.bitwarden.com/app-id.json"), "vault.bitwarden.com");
-        map.insert(from_url("https://keepersecurity.com"), "keepersecurity.com");
-        map.insert(from_url("https://api-9dcf9b83.duosecurity.com"), "duosecurity.com");
-        map.insert(from_url("https://dashboard.stripe.com"), "dashboard.stripe.com");
-        map.insert(from_url("https://id.fedoraproject.org/u2f-origins.json"), "id.fedoraproject.org");
-        map.insert(from_url("https://lastpass.com"), "lastpass.com");
-        map.insert(from_url("https://u2f.aws.amazon.com/app-id.json"), "aws.amazon.com");
-
-        // Additional known app IDs not yet in KnownFacets.swift
         map.insert(from_url("bin.coffee"), "bin.coffee");
         map.insert(from_url("coinbase.com"), "coinbase.com");
         map.insert(from_url("demo.yubico.com"), "demo.yubico.com");
+        map.insert(
+            from_url("https://api-9dcf9b83.duosecurity.com"),
+            "duosecurity.com",
+        );
+        map.insert(
+            from_url("https://dashboard.stripe.com"),
+            "dashboard.stripe.com",
+        );
+        map.insert(from_url("https://demo.yubico.com"), "demo.yubico.com");
+        map.insert(
+            from_url("https://github.com/u2f/trusted_facets"),
+            "github.com",
+        );
         map.insert(from_url("https://gitlab.com"), "gitlab.com");
+        map.insert(
+            from_url("https://id.fedoraproject.org/u2f-origins.json"),
+            "id.fedoraproject.org",
+        );
+        map.insert(from_url("https://keepersecurity.com"), "keepersecurity.com");
+        map.insert(from_url("https://lastpass.com"), "lastpass.com");
         map.insert(from_url("https://mdp.github.io"), "mdp.github.io");
+        map.insert(
+            from_url("https://u2f.aws.amazon.com/app-id.json"),
+            "aws.amazon.com",
+        );
         map.insert(from_url("https://u2f.bin.coffee"), "u2f.bin.coffee");
+        map.insert(
+            from_url("https://vault.bitwarden.com/app-id.json"),
+            "vault.bitwarden.com",
+        );
+        map.insert(
+            from_url("https://www.dropbox.com/u2f-app-id.json"),
+            "www.dropbox.com",
+        );
         map.insert(from_url("https://www.fastmail.com"), "www.fastmail.com");
+        map.insert(
+            from_url("https://www.gstatic.com/securitykey/origins.json"),
+            "google.com",
+        );
+        map.insert(from_url("ssh:"), "ssh key");
         map.insert(from_url("webauthn.bin.coffee"), "webauthn.bin.coffee");
         map.insert(from_url("webauthn.io"), "webauthn.io");
-        map.insert(from_url("ssh:"), "ssh key");
-
+        map.insert(from_url("www.token2.com"), "www.token2.com");
         map
     };
 }
